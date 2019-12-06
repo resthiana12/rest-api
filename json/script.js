eee2047299905bfd9c33d4534215c1d260681d2b
resthiana12 <resthiana123@gmail.com>
2019-12-07 06:29:31 +0700

    mengedit file script.js

---
+++ json/script.js
@@ -0,0 +1,12 @@
+//ini adalah objek
+let mahasiswa = {
+    nama : "Resthiana",
+    nrp : "173040057",
+    email : "resthiana123@gmail.com"
+}
+
+//hasilnya objek 
+//console.log(mahasiswa);
+
+//mengubah objek jadi json
+console.log(JSON.stringify(mahasiswa));
