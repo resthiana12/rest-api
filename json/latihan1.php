cb1d670c631ce01065b65db7308e5e58351a1b91
resthiana12 <resthiana123@gmail.com>
2019-12-06 12:58:34 +0700

    Konek ke database kemudian di encode jadi JSON

--- json/latihan1.php
+++ json/latihan1.php
@@ -1,10 +1,22 @@
 <?php
 
-$mahasiswa=[
-    "nama" => "Resthiana",
-    "nrp" => "173040057",
-    "email" => "resthiana123@gmail.com"
-];
+// $mahasiswa=[
+//     [
+//         "nama" => "Resthiana",
+//         "nrp" => "173040057",
+//         "email" => "resthiana123@gmail.com"
+//     ],
+//     [
+//         "nama" => "Tawfeeq Aatef",
+//         "nrp" => "143020057",
+//         "email" => "tawfeeqA123@gmail.com"
+//     ]
+// ];
+
+$dbh = new PDO('mysql:host=localhost; dbname=phpdasar', 'root', '');
+$db = $dbh->prepare('SELECT * FROM mahasiswa');
+$db->execute();
+$mahasiswa = $db->fetchAll(PDO::FETCH_ASSOC);
 
 $data = json_encode($mahasiswa);
 echo $data;
