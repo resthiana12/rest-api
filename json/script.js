db6fcb5a2f4879ba8c7cda310828381b2d1085c9
resthiana12 <resthiana123@gmail.com>
2019-12-07 06:41:44 +0700

    mengubah json jadi objek di script.js

--- json/script.js
+++ json/script.js
@@ -1,12 +1,23 @@
 //ini adalah objek
-let mahasiswa = {
-    nama : "Resthiana",
-    nrp : "173040057",
-    email : "resthiana123@gmail.com"
-}
+// let mahasiswa = {
+//     nama : "Resthiana",
+//     nrp : "173040057",
+//     email : "resthiana123@gmail.com"
+// }
 
 //hasilnya objek 
 //console.log(mahasiswa);
 
 //mengubah objek jadi json
-console.log(JSON.stringify(mahasiswa));
+//console.log(JSON.stringify(mahasiswa));
+
+//mengubah json jadi objek mah pake ajax
+//panggil objek ajax
+let xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function(){
+    if(xhr.readyState == 4 & xhr.status == 200){
+        let mahasiswa = this.responseText;
+        console.log(mahasiswa);
+    }
+}
+xhr.open('GET', 'coba.json', true);
